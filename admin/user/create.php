0ae5d57470c78925374ba32cf21123056df17fc5
nguyenngocm <mynn@students.hueic.edu.vn>
2022-12-21 21:05:07 +0700

    create

---
+++ admin/user/create.php
@@ -0,0 +1,14 @@
+<?php
+require_once "../../core/function.php";
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$role = $_POST['role'];
+
+
+$rs = create_user($email,$password,$role);
+
+$response = array(
+    'status' => $rs
+);
+echo json_encode($response);
